47ee265b0ecb08cb5c94ffd3e7b44c239ce2f556
YanaKudrinskaya <yanakudrinskaya86@gmail.com>
2026-04-29 15:47:26 +0300

    amvera

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,6 +5,7 @@ import org.jetbrains.exposed.sql.Database
 import io.github.cdimascio.dotenv.dotenv
 import io.ktor.server.engine.embeddedServer
 import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
 import ru.playzone.database.games.Games
 import ru.playzone.database.tokens.Tokens
 import ru.playzone.database.users.Users
@@ -34,7 +35,10 @@ fun main(args: Array<String>) {
         user = dbUser,
         password = dbPassword
     )
-    SchemaUtils.create(Users, Tokens, Games)
+    transaction {
+        SchemaUtils.create(Users, Tokens, Games)
+        println("Tables created successfully")
+    }
 
     embeddedServer(CIO, port = serverPort, host = "0.0.0.0") {
         rootModule()
